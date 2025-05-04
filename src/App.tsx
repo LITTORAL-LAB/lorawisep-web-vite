ee9edd42993b86520134ad61b4a722656a4e503d
Pedro <92474347+Pedroffda@users.noreply.github.com>
2025-05-04 20:59:11 -0300

    feat: initialize project with React, TypeScript, and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
